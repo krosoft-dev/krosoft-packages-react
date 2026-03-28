3b9d4cd811543ebb4cacb66e42bb91d903d29cc2
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-03-28 03:00:20 +0100

    Ajout tailwind.helper

---
+++ tests/helpers/tailwind.helper.test.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest";
+import { cn } from "../../src/helpers/tailwind.helper";
+
+describe("cn", () => {
+  it("fusionne des classes simples", () => {
+    expect(cn("foo", "bar")).toBe("foo bar");
+  });
+
+  it("résout les conflits tailwind", () => {
+    expect(cn("p-4", "p-2")).toBe("p-2");
+  });
+
+  it("ignore les valeurs falsy", () => {
+    expect(cn("foo", undefined, null, false, "bar")).toBe("foo bar");
+  });
+
+  it("gère les classes conditionnelles", () => {
+    expect(cn("foo", { bar: true, baz: false })).toBe("foo bar");
+  });
+});
