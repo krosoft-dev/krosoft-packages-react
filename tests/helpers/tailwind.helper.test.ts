252b642885c1aa72893b5267cfef7db0205ebf76
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-03-31 22:49:11 +0200

    Ajout jsdom + vitest.config avec alias @/ pour les tests

--- tests/helpers/tailwind.helper.test.ts
+++ tests/helpers/tailwind.helper.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { cn } from "../../src/helpers/tailwind.helper";
+import { cn } from "@/helpers/tailwind.helper";
 
 describe("cn", () => {
   it("fusionne des classes simples", () => {
