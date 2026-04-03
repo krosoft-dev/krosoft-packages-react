32684193b535339093e980e7ebe08abeed4dc2a2
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-04-04 01:16:49 +0200

    Rework useNotifications

--- src/hooks/ui/useNotifications.ts
+++ src/hooks/ui/useNotifications.ts
@@ -1,26 +1,18 @@
-import { useToast } from "./useToast";
+import { toast } from "./useToast";
 
-export const useNotifications = () => {
-  const { toast } = useToast();
+export interface UseNotificationsResult {
+  showSuccess: (title: string, description?: string) => void;
+  showError: (title: string, description?: string, error?: Error | null) => void;
+}
 
-  const showSuccess = (title: string, description?: string | undefined) => {
-    toast({
-      title: title,
-      description: description,
-    });
+export const useNotifications = (): UseNotificationsResult => {
+  const showSuccess = (title: string, description?: string): void => {
+    toast({ title, description });
   };
 
-  const showError = (
-    title: string,
-    description?: string | undefined,
-    error?: Error | null,
-  ) => {
+  const showError = (title: string, description?: string, error?: Error | null): void => {
     console.error(description, error);
-    toast({
-      title: title,
-      description: description,
-      variant: "destructive",
-    });
+    toast({ title, description, variant: "destructive" });
   };
 
   return {
