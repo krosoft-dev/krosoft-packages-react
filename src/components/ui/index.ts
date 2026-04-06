58b19d2e033a7b732ded39906697cc07a8c2431e
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-04-06 12:17:43 +0200

    export Button component from ui index

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,6 @@
 export { Alert, AlertTitle, AlertDescription } from "./alert";
+export { Button, buttonVariants } from "./button";
+export type { ButtonProps } from "./button";
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./card";
 export { Badge, badgeVariants } from "./badge";
 export type { BadgeProps } from "./badge";
