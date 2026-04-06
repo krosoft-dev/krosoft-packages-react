e4cecdd36ac14bd94f1da05395e407d125aa1b53
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-04-06 12:16:00 +0200

    Add Button component

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-
 import { cn } from "@krosoft/react/helpers";
 
 const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
