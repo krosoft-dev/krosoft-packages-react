61d580d97fb0ec46287c722174afeefc85ecf9cb
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-04-17 10:12:34 +0200

    Add ToastActionElement to showSuccess

--- src/hooks/ui/useNotifications.ts
+++ src/hooks/ui/useNotifications.ts
@@ -1,13 +1,14 @@
 import { toast } from "./useToast";
+import type { ToastActionElement } from "../../components/ui";
 
 export interface UseNotificationsResult {
-  showSuccess: (title: string, description?: string) => void;
+  showSuccess: (title: string, description?: string, action?: ToastActionElement) => void;
   showError: (title: string, description?: string, error?: Error | null) => void;
 }
 
 export const useNotifications = (): UseNotificationsResult => {
-  const showSuccess = (title: string, description?: string): void => {
-    toast({ title, description });
+  const showSuccess = (title: string, description?: string, action?: ToastActionElement): void => {
+    toast({ title, description, action });
   };
 
   const showError = (title: string, description?: string, error?: Error | null): void => {
