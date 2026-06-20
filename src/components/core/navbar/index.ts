eb615297705b693472038d0997930155aa574b95
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-06-20 19:02:36 +0200

    merge

--- src/components/core/navbar/index.ts
+++ src/components/core/navbar/index.ts
@@ -1,9 +1,8 @@
 export { Sidebar, SidebarProvider } from "./Sidebar";
-export type { SidebarGroup, SidebarItem, SidebarProps } from "./Sidebar";
-export type { SidebarProps, SidebarItem, SidebarGroup, SidebarSubItem, SidebarSlots, SidebarSearch } from "./Sidebar";
+export type { SidebarGroup, SidebarGroup, SidebarItem, SidebarItem, SidebarProps, SidebarProps, SidebarSearch, SidebarSlots, SidebarSubItem } from "./Sidebar";
 export { SidebarHeader } from "./SidebarHeader";
 export type { SidebarHeaderProps } from "./SidebarHeader";
 export { SidebarNavItem } from "./SidebarNavItem";
 export type { SidebarNavItemProps } from "./SidebarNavItem";
 export { Topbar } from "./Topbar";
-export { useSidebar, SidebarContext } from "./useSidebar";
+export { SidebarContext, useSidebar } from "./useSidebar";
